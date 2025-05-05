aad7e5388a437d4da8219e6d0c866e9bf60f69d8
Irtaza2009 <irtazanaqvi05@gmail.com>
2025-05-05 22:41:43 +0500

    fixed bug

--- arena-backend/routes/auth.js
+++ arena-backend/routes/auth.js
@@ -68,7 +68,7 @@ router.get("/slack/callback", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24 * 7,
     });
 
