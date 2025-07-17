4b85fcf63ef3d5d705aa4307fad3b5ea16b042dc
Irtaza2009 <irtazanaqvi05@gmail.com>
2025-07-18 01:09:11 +0500

    cors bugs

--- arena-backend/routes/auth.js
+++ arena-backend/routes/auth.js
@@ -68,7 +68,7 @@ router.get("/slack/callback", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: /*process.env.NODE_ENV === "production" ?*/ "none", //: "lax",
       maxAge: 1000 * 60 * 60 * 24 * 7,
     });
 
